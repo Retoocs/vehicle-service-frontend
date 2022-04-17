e21e52fcc7abf08f779cf0b2b7845d54d8cd2b33
Matej <matejchvostek@gmail.com>
2022-04-17 17:12:51 +0200

    feat: refactor createNewCase()

--- src/app/views/side-nav/repairs/content/1/side-nav-repairs-content1-case-view.component.ts
+++ src/app/views/side-nav/repairs/content/1/side-nav-repairs-content1-case-view.component.ts
@@ -1,21 +1,23 @@
 import {AfterViewInit, Component, Inject, ViewChild} from '@angular/core';
 import {
   AllowedNetsService,
-  AllowedNetsServiceFactory,
+  AllowedNetsServiceFactory, Case, CaseResourceService,
   CaseViewService,
-  CategoryFactory,
-  defaultCaseSearchCategoriesFactory,
+  CategoryFactory, CreateCaseEventOutcome,
+  defaultCaseSearchCategoriesFactory, EventOutcomeMessageResource,
   InjectedTabbedCaseViewData,
   LoggerService,
   NAE_BASE_FILTER,
   NAE_SEARCH_CATEGORIES,
-  NAE_TAB_DATA,
+  NAE_TAB_DATA, ProcessService,
   SearchService,
-  SimpleFilter,
+  SimpleFilter, SnackBarService,
   TabbedCaseView,
   ViewIdService,
 } from '@netgrif/components-core';
 import {HeaderComponent} from '@netgrif/components';
+import {Observable, Subject} from 'rxjs';
+import {TranslateService} from '@ngx-translate/core';
 
 const localAllowedNetsFactory = (factory: AllowedNetsServiceFactory) => {
   return factory.createFromArray(['repair']);
@@ -56,6 +58,10 @@ export class SideNavRepairsContent1CaseViewComponent extends TabbedCaseView impl
 
   constructor(caseViewService: CaseViewService,
               loggerService: LoggerService,
+              private _processService: ProcessService,
+              private _caseResourceService: CaseResourceService,
+              private _snackBarService: SnackBarService,
+              private _translate: TranslateService,
               @Inject(NAE_TAB_DATA) injectedTabData: InjectedTabbedCaseViewData) {
     super(caseViewService, loggerService, injectedTabData, undefined, undefined, undefined, {
       enableCaseTitle: true,
@@ -65,6 +71,40 @@ export class SideNavRepairsContent1CaseViewComponent extends TabbedCaseView impl
 
   ngAfterViewInit(): void {
     super.initializeHeader(this.caseHeaderComponent);
+  }
+
+  createNewCase(): Observable<Case> {
+    const myCase = this.createInstance('repair');
+    myCase.subscribe( kaze => {
+      if (this._caseViewService.viewEnabled(kaze)) {
+        this.openTab(kaze);
+      }
+    });
+    return myCase;
+  }
+
+  private createInstance(petriNetId: string): Observable<Case> {
+    let msgObservable: Observable<EventOutcomeMessageResource>;
+    const myCase = new Subject<Case>();
+
+    this._processService.getNet(petriNetId).subscribe(net => {
+      const newCase = {
+        title: net.defaultCaseName,
+        color: 'panel-primary-icon',
+        netId: net.stringId
+      };
+
+      msgObservable = this._caseResourceService.createCase(newCase);
+      msgObservable.subscribe((response: EventOutcomeMessageResource) => {
+        this._snackBarService.openSuccessSnackBar(this._translate.instant('side-menu.new-case.createCase')
+          + ' ' + this._translate.instant('side-menu.new-case.defaultCaseName'));
+        this._caseViewService.reload();
+        myCase.next((response.outcome as CreateCaseEventOutcome).aCase);
+        myCase.complete();
+      }, error => this._snackBarService.openErrorSnackBar(error.message ? error.message : error));
+
+    });
+    return myCase.asObservable();
   }
 
 }
