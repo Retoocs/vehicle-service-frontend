25f38df59617c7e5b4bd50e31a3c1c5cf53ae45b
Matej <matejchvostek@gmail.com>
2022-04-28 10:58:13 +0200

    remove tab

--- src/app/views/side-nav/allCases/allcases.component.ts
+++ src/app/views/side-nav/allCases/allcases.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {NAE_VIEW_ID_SEGMENT, ViewIdService} from '@netgrif/components-core';
 import {SideNavAllcasesContent0TaskViewComponent} from './content/0/side-nav-allcases-content0-task-view.component';
 import {SideNavAllcasesContent1CaseViewComponent} from './content/1/side-nav-allcases-content1-case-view.component';
-import {SideNavAllcasesContent2CaseViewComponent} from './content/2/side-nav-allcases-content2-case-view.component';
 
 
 @Component({
@@ -32,20 +31,7 @@ export class AllcasesComponent {
         tabViewComponent: SideNavAllcasesContent0TaskViewComponent,
         tabViewOrder: 0
       }
-    },
-    {
-      label: {
-        icon: 'face',
-        text: 'My Cases',
-      },
-      canBeClosed: false,
-      tabContentComponent: SideNavAllcasesContent2CaseViewComponent,
-      order: -1,
-      injectedObject: {
-        tabViewComponent: SideNavAllcasesContent0TaskViewComponent,
-        tabViewOrder: 0
-      }
-    },
+    }
   ];
 
 }
